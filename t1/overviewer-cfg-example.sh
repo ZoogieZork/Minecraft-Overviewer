37dd34d362377b1629850ca769d8b15a9ba42871
Michael Imamura <zoogie@lugatgt.org>
2010-12-23 18:04:57 -0500

    Added example OVERVIEWER_CFG file.

---
+++ t1/overviewer-cfg-example.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+WORK_DIR="$HOME/minecraft"
+WORLD_URL='http://example.org/world.7z'
+WWW_DIR=/var/www/example.org/minecraft
+
+OVERVIEWER_ROOT="$WORK_DIR/bin"
+
+WORLD="$WORK_DIR/world"
+WORLD_DIR="$WORK_DIR/world-map"
+WORLD_NIGHT_DIR="$WORK_DIR/world-map-night"
+
